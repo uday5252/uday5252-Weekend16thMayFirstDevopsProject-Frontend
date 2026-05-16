8a839ca22dee127c712a18e8837bb3b653a0835b
uday kiran <uk144199@gmail.com>
2026-05-16 21:05:16 +0530

    first commit of frontend

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/message')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div style={{ padding: '40px', fontFamily: 'Arial' }}>
+      <h1>React Frontend</h1>
+
+      <h2>Message from Backend:</h2>
+
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;

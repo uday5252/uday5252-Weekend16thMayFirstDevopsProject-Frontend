2ea6c7e908a7f916b9abfc0f5f011d93c30d1ece
uday5252 <73519118+uday5252@users.noreply.github.com>
2026-05-30 21:35:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 function App() {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/message')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-      .catch(err => console.log(err));
-  }, []);
+ useEffect(() => {
+  fetch('http://a84f04537361a44d6b3b4d69fdc1e12d-1184505678.ap-south-1.elb.amazonaws.com/api/message')
+    .then(res => res.json())
+    .then(data => setMessage(data.message))
+    .catch(err => console.log(err));
+}, []);
 
   return (
     <div style={{ padding: '40px', fontFamily: 'Arial' }}>
